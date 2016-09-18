fd5588011891a0a15f3cf2de012c9a112b769860
James <lim.en.hui.james@gmail.com>
2016-09-18 08:01:19 +0800

    Updated osuBeatmap to contain more useful functions

--- osuBeatmap.cpp
+++ osuBeatmap.cpp
@@ -1,5 +1,6 @@
 #include "osuBeatmap.h" //requires this to work
 
+/* Finds out if the file given is an Mp3 */
 bool osuBeatmapFunctions::isMusicMp3(string s_string)
 {
 	if (s_string.rfind(".mp3") != string::npos && s_string.rfind(".mp3") == s_string.size() + 1 - 4)
@@ -8,12 +9,33 @@ bool osuBeatmapFunctions::isMusicMp3(string s_string)
 	return false; //default return
 }
 
+/* Finds out if the osuBeatmap structure given is an Mp3 */
 bool osuBeatmapFunctions::isMusicMp3(osuBeatmap ob)
 {
 	return isMusicMp3(ob.MusicLocation); //use already defined function
 }
 
+/* Find duplicates of beatmaps */
 void osuBeatmapFunctions::fixBeatmapDuplicates(vector<osuBeatmap> &vec_)
 {
+	/*Memory buffer of the members to compare*/
+	osuBeatmap ob_Buffer;
+	for (unsigned int iii = 0; iii < vec_.size(); iii++)
+	{
+		if (ob_Buffer.BeatmapName == vec_[iii].BeatmapName 
+			|| ob_Buffer.BackgroundPhoto == vec_[iii].BackgroundPhoto
+			|| ob_Buffer.MusicLocation == vec_[iii].MusicLocation)
+		{
+			vec_[iii] = osuBeatmap{"","",""}; //empty it out first ()
+		}
+		ob_Buffer = vec_[iii]; //Simple and easy
+	}
 
+	/*This loop will remove all of the osuBeatmap{"","",""} from the vector*/
+	for (unsigned int iii = 0; iii < vec_.size(); iii++) //vec_ size changes in the loop
+	{
+		if (vec_[iii] == osuBeatmap{"","",""})
+			vec_.erase(vec_.begin() + iii); //erases the element
+	}
+	//end of function
 }
--- osuBeatmap.h
+++ osuBeatmap.h
@@ -13,6 +13,30 @@ struct osuBeatmap
 	string BeatmapName; //BeatmapName, will be used in the song title
 	string BackgroundPhoto; //background photo, will be processed and cropped
 	string MusicLocation; //music, will be tagged
+
+	bool operator==(const osuBeatmap& other)
+	{
+		return (this->BeatmapName == other.BeatmapName
+			&& this->BackgroundPhoto == other.BackgroundPhoto
+			&& this->MusicLocation == other.MusicLocation);
+	}
+
+	osuBeatmap& operator=(osuBeatmap other) //this constructor requires a copy of an object
+	{
+		swap(*this, other); //swaps this with the object given
+		return *this;
+	}
+
+	friend void swap(osuBeatmap& LHS, osuBeatmap& RHS) //this won't throw an exception
+	{
+		//Enable ADL (it's good practice, apparently) //Also, ADL's needed to find this function
+		using std::swap;
+
+		//swap members of the two classes
+		swap(LHS.BeatmapName, RHS.BeatmapName);
+		swap(LHS.BackgroundPhoto, RHS.BackgroundPhoto);
+		swap(LHS.MusicLocation, RHS.MusicLocation);
+	}
 };
 
 /* This structure contains the functions that is used to manipulate the osuBeatmaps*/
