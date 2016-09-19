fe1d83ccc4f5f070b74c5f148bf1c8c40c7621de
James <lim.en.hui.james@gmail.com>
2016-09-19 21:34:00 +0800

    Command handler can now parse commands (WIP)

--- commandHandler.cpp
+++ commandHandler.cpp
@@ -5,14 +5,35 @@ CommandHandler::CommandHandler()
 	//ctor
 }
 
-CommandHandler::CommandHandler(std::string s, wxRichTextCtrl *c)
+CommandHandler::CommandHandler(std::string s, ProgressTracker *pt)
 {
-	processCommand(s, c);
+	processCommand(s, pt);
 }
 
-void CommandHandler::processCommand(std::string command, wxRichTextCtrl *console)
+void CommandHandler::processCommand(std::string command, ProgressTracker *pt)
 {
 	//Process Command.
+	using namespace Commands;
+
+	if (commandsAvailable.count(command) != 0 && commandsAvailable.count(command) == 1)
+	{
+		//launches the command
+		commandsAvailable[command]; //plays the command
+		return;
+	}
+	if (commandsAvailable.count(command) == 0)
+	{
+		//report to progress tracker
+		pt << "Error processing command: Command not found";
+		return;
+	}
+	if (commandsAvailable.count(command) > 1)
+	{
+		//report to progress tracker
+		pt << "Error processing command: Lazy programmer forgot to check code";
+		return;
+	}
+	pt << "Error processing command: EOF reached.";
 }
 
 CommandHandler::~CommandHandler()
--- commandHandler.h
+++ commandHandler.h
@@ -2,21 +2,25 @@
 #define commandHandlerHEADER
 
 #include <wx/richtext/richtextctrl.h>
+#include <map>
+
+#include "ProgressTracker.h"
 
 /* Namespace that contains commands to test the program */
 namespace Commands
 {
 	typedef void (*commandFunction)();
+	const std::map<std::string, commandFunction> commandsAvailable = {}; //pretty cool, right?
 };
 
 class CommandHandler
 {
 public:
 	CommandHandler();
-	CommandHandler(std::string, wxRichTextCtrl*);
+	CommandHandler(std::string, ProgressTracker*);
 	~CommandHandler();
 
-	void processCommand(std::string, wxRichTextCtrl*);
+	void processCommand(std::string, ProgressTracker*);
 protected:
 private:
 	void operator=(CommandHandler&){} //do not implement
