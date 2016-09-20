fb33cc84f4ccf5e3aefd9f8cb075c5fc67fb702b
James <lim.en.hui.james@gmail.com>
2016-09-20 19:10:37 +0800

    Added Arguments class

--- commandHandler.cpp
+++ commandHandler.cpp
@@ -1,5 +1,6 @@
 #include "commandHandler.h"
 
+/* Needed to make the whole file work (Declaration of Commands) */
 namespace Commands
 {
 	std::map<std::string, commandFunction> commandsAvailable = 
@@ -8,6 +9,7 @@ namespace Commands
 	}; //pretty cool, right?
 }
 
+/* Command Handler */
 CommandHandler::CommandHandler()
 {
 	//ctor
@@ -55,6 +57,54 @@ CommandHandler::~CommandHandler()
 	//dtor
 }
 
+/* Arguments */
+Arguments::Arguments(){ }; //NoConstruct
+Arguments::~Arguments() { }; //Don't have to destroy anything here
+
+Arguments::Arguments(std::string s)
+{
+	//sets the full argument
+	setFullArgument(s);
+}
+
+void Arguments::setFullArgument(std::string s)
+{
+	s_fullArgument = s; //sets the full argument
+	splitArguments(); //splits the arguments into meaningful arguments
+}
+
+std::string Arguments::getFullArgument()
+{
+	return s_fullArgument;
+}
+
+std::string Arguments::getArgument(int iii)
+
+{
+	return v_Arguments[iii];
+}
+
+void Arguments::setArgument(int iii, std::string s)
+{
+	v_Arguments[iii] = s; //very simple and fast way (error handling and everything done by vector class)
+}
+
+void Arguments::splitArguments()
+{
+	//Begin spliting arguments
+	std::string buffer = s_fullArgument; //sets full argument
+
+	int n_lastPosition = 0; //sets last position
+	while (buffer.find(" ", n_lastPosition) != std::string::npos) //this is the while loop
+	{
+		v_Arguments.push_back(buffer.substr(n_lastPosition, buffer.find(" ", n_lastPosition) - n_lastPosition)); //pushes back this anonymous string
+		n_lastPosition = buffer.find(" ", n_lastPosition); //sets n_lastPosition to the latest position
+	}
+
+	//Done.
+}
+
+/* Commands */
 void Commands::printAllCommands(ProgressTracker* pt)
 {
 	for (std::map<std::string, commandFunction>::iterator it = commandsAvailable.begin(); it != commandsAvailable.end(); ++it)
--- commandHandler.h
+++ commandHandler.h
@@ -3,6 +3,7 @@
 
 #include <wx/richtext/richtextctrl.h>
 #include <map>
+#include <vector>
 
 #include "ProgressTracker.h"
 
@@ -18,6 +19,43 @@ protected:
 private:
 	void operator=(CommandHandler&){} //do not implement
 	CommandHandler(CommandHandler&){} //do not implement
+};
+
+class Arguments
+{
+public:
+	Arguments();
+	Arguments(std::string);
+	~Arguments();
+
+	Arguments(Arguments &a)
+	{*this = a;} //super fast constructor
+
+	Arguments& operator=(Arguments RHS)
+	{
+		swap(*this, RHS); //swaps
+		return *this;
+	}
+
+	friend void swap(Arguments& LHS, Arguments& RHS)
+	{
+		using std::swap; //ADL
+
+		swap(LHS.s_fullArgument, RHS.s_fullArgument);
+		swap(RHS.v_Arguments, RHS.v_Arguments);
+	}
+
+	void setFullArgument(std::string);
+	std::string getFullArgument();
+	std::string getArgument(int);
+	void setArgument(int, std::string);
+
+protected:
+private:
+	std::string s_fullArgument 				= "";
+	std::vector<std::string> v_Arguments 	= {};
+
+	void splitArguments();
 };
 
 /* Namespace that contains commands to test the program */
